a4052953b724c27f4e2428d28d9212afc83932d0
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-03-27 14:38:58 -0300

    finished omnistack week 11

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const { celebrate, Segments, Joi } = require('celebrate')
 
 const OngController = require('./controllers/ong.controller')
 const IncidentController = require('./controllers/incident.controller')
@@ -9,12 +10,33 @@ const SessionController = require('./controllers/session.controller')
 routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.list)
-routes.post('/ongs', OngController.create)
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create)
 
-routes.post('/incidents', IncidentController.create)
-routes.get('/incidents', IncidentController.list)
-routes.delete('/incidents/:id', IncidentController.delete)
+routes.post('/incidents',  IncidentController.create)
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}), IncidentController.list)
 
-routes.get('/profile', ProfileController.get)
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete)
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.get)
 
 module.exports = routes
